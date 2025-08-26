fd8dda9b1c2d5e53d5e64a99b0b6db65f1ac6f63
Warley Oliveira <softwarley@hotmail.com>
2025-08-26 15:38:38 -0300

    add sw

--- sw.js
+++ sw.js
@@ -1,13 +1,62 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "tj-report-cache";
+
+const urlsToCache = ["/", "/index.html", "/app.js"];
+
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open("app-cache").then(cache => {
-      return cache.addAll(["/", "/index.html", "/app.js"]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("fetch", (e) => {
+  caches.match(e.request).then((response) => {
+    if (response) {
+      fetch(e.request).then((networkResponse) => {
+        if (networkResponse.ok) {
+          const cacheClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(e.request, cacheClone);
+          });
+        }
+      });
+
+      return response;
+    } else {
+      return fetch(e.request)
+        .then((networkResponse) => {
+          if (networkResponse.ok) {
+            const cacheClone = networkResponse.clone();
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(e.request, cacheClone);
+            });
+          }
+          return networkResponse;
+        })
+        .catch(() => {
+          return new Response(
+            "Você está offline e o recurso não está em cache.",
+            {
+              status: 503,
+              statusText: "Service Unavailable",
+            }
+          );
+        });
+    }
+  });
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
